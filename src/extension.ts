e7a8dfc2278b5dc5003c3f33e10e1c6fc183720c
Santiago Zibecchi <santiago.zibe@gmail.com>
2023-03-07 18:46:46 -0300

    sombreado funcionando

--- src/extension.ts
+++ src/extension.ts
@@ -1,13 +1,26 @@
 import * as vscode from 'vscode';
 
 export function activate(context: vscode.ExtensionContext) {
-	console.log('Congratulations, your extension "ooo" is now active!');
+	console.log('Congratulations, your extension "i18nextLightingKeys" is now active!');
 
 	const decorationType = vscode.window.createTextEditorDecorationType({
 		backgroundColor: 'rgba(255, 255, 0, 0.1)',
 	});
 
 	let activeEditor = vscode.window.activeTextEditor;
+	let disableShading = vscode.workspace.getConfiguration().get<boolean>('i18nextLightingKeys.disableShading');
+
+	context.subscriptions.push(
+		vscode.commands.registerCommand('i18nextLightingKeys.disableShading', () => {
+			vscode.workspace.getConfiguration().update('i18nextLightingKeys.disableShading', true, true);
+		})
+	);
+
+	context.subscriptions.push(
+		vscode.commands.registerCommand('i18nextLightingKeys.enableShading', () => {
+			vscode.workspace.getConfiguration().update('i18nextLightingKeys.disableShading', false, true);
+		})
+	);
 
 	if (activeEditor) {
 		triggerUpdateDecorations();
@@ -43,28 +56,37 @@ export function activate(context: vscode.ExtensionContext) {
 		const text = activeEditor.document.getText();
 		let ranges: vscode.Range[] = [];
 
-		// Buscar texto entre comillas "", '' y ``
-		const attributeRegEx = /(["'`])(?:(?=(\\?))\2.)*?\1/g;
+		if (!disableShading) {
+			// Buscar texto entre comillas "", '' y ``
+			const attributeRegEx = /(["'`])(?:(?=(\\?))\2.)*?\1/g;
 
-		let match;
-		while ((match = attributeRegEx.exec(text))) {
-			const startPos = activeEditor.document.positionAt(match.index + match[0].indexOf(match[1]) + 1);
-			const endPos = activeEditor.document.positionAt(match.index + match[0].lastIndexOf(match[1]));
-			const range = new vscode.Range(startPos, endPos);
-			ranges.push(range);
+			let match;
+			while ((match = attributeRegEx.exec(text))) {
+				const startPos = activeEditor.document.positionAt(match.index + match[0].indexOf(match[1]) + 1);
+				const endPos = activeEditor.document.positionAt(match.index + match[0].lastIndexOf(match[1]));
+				const range = new vscode.Range(startPos, endPos);
+				ranges.push(range);
+			}
 		}
-		
+
 		activeEditor.setDecorations(decorationType, ranges);
-		
+
 		context.subscriptions.push(vscode.commands.registerCommand('i18nextLightingKeys.selectKeys', () => {
 			vscode.window.activeTextEditor!.selections = ranges.map(range => new vscode.Selection(range.start, range.end));
-			vscode.window.showInformationMessage('Claves seleccionadas!');
+			vscode.window.showInformationMessage('Textos sombreados seleccionadas!');
 		}));
 	}
 
+	vscode.workspace.onDidChangeConfiguration((event) => {
+		if (event.affectsConfiguration('i18nextLightingKeys.disableShading')) {
+			disableShading = vscode.workspace.getConfiguration().get<boolean>('i18nextLightingKeys.disableShading');
+			triggerUpdateDecorations();
+		}
+	});
+
 	context.subscriptions.push(vscode.commands.registerCommand('i18nextLightingKeys.markingTS', () => {
-		vscode.window.showInformationMessage('Sombreando claves!');
+		vscode.window.showInformationMessage('Extensión inicializada! Se ha sombreado el texto..');
 	}));
 }
 
-export function deactivate() {}
+export function deactivate() { }
